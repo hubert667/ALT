993fcc136cdc152577904b76976107fe7be37b26
Sander <sander@sander-K52JB.(none)>
2013-10-28 10:51:47 +0100

    added stacks and a few other things

--- ALTOne/decoder.py
+++ ALTOne/decoder.py
@@ -58,9 +58,9 @@ class Graph:
     beam_width = 0
     nodes = []
     node_map = {} #maps indexes from nodes to their parents (sort of like pointers I guess?)
+    node_stacks = [] #a list of stacks of node_id's. If time left, could replace nodes
     equiv_nodes = {} #maps the best node to its equivalents
     expanded = True #This shows if the graph can still be expanded
-    
 
     def __init__(self, source_phrase, beam_width,language_model,l1_given_source,source_given_l1,max_phrase_length):
         self.l1_given_source=l1_given_source
@@ -69,27 +69,30 @@ class Graph:
         self.max_phrase_length=max_phrase_length
         self.source_phrase = source_phrase
         self.beam_width = beam_width
+        sentence_len = len(source_phrase.split())
+        self.node_stacks = [[]]*sentence_len
 
     def expand_graph(self):
         '''
         This loops through nodes to expand/collapse them
         '''
-        self.expanded = False
-        for n_id in xrange(len(self.nodes)):
-            n = self.nodes[n_id]
-            if not n.collapsed:
-                self.collapse_node(n_id)
-                self.expanded = True
-            if not n.stopped:
-                self.expand_node(n_id)
-                self.expanded = True
+        #self.expanded = False
+        #for n_id in xrange(len(self.nodes)):
+        for stack in self.node_stacks:
+            for n in stack:
+                if not n.collapsed:
+                    self.collapse_node(n_id)
+                    #self.expanded = True
+                if not n.stopped:
+                    self.expand_node(n_id)
+                    #self.expanded = True
 
     def expand_node(self, node_id):
         '''
         This method creates new nodes from node
         '''
-        node = self.nodes[node_id]
-        self.nodes[node_id] = node 
+        #node = self.nodes[node_id]
+        #self.nodes[node_id] = node 
         return 0
 
     def collapse_node(self, node_id):
@@ -97,12 +100,13 @@ class Graph:
         This method finds nodes equivalent to node and makes pointers to them
         Don't know if this should be in here, or maybe at the graph level?
         '''
-        node1 = self.nodes[node_id]
-        for n_id in xrange(len(self.nodes)):
-            node2 = self.nodes[n_id]
-            #if NODES EQUIVALENT:
+        node1 = self.node_stacks[node_id[0]][node_id[1]]
+        for n2 in xrange(len(self.node_stacks[node_id[0]])):
+            n_id = (node_id[0], n2) 
+            node2 = self.nodes[n_id[0]][n_id[1]]
             if node1.already_translated == node2.already_translated and\
-                node1.current_position_translation == node2.current_position_translation:
+                node1.current_position_translation == node2.current_position_translation and\
+                node1.last_history == node2.last_history:
                 if node2 in self.equiv_nodes:
                     if node2.probability >= node1.probability:
                         self.equiv_nodes[n_id].append(node_id)
@@ -115,7 +119,6 @@ class Graph:
                         self.equiv_nodes[n_id] = [node_id]
                     else:
                         self.equiv_nodes[node_id] = [n_id]
-        return 0
     
     def calculate_translation(self):
         
@@ -132,7 +135,7 @@ class Graph:
             for local_translation in translations:
                 node=Node(local_translation,start_node,[i,i])
                 node.calculate_probability(self.ngrams, self.l1_given_source, self.source_given_l1,local_translation)
-                self.nodes.append(node)
+                self.node_stacks[0].append(node)
         return self.destination_phrase
     
     def create_coverage_vector(self,number):
@@ -141,10 +144,20 @@ class Graph:
             array.append(0)
         return array
 
+    def compute_future_cost(i,j):
+        source = self.source_phrase.split()[i:j+1]
+        if j-i =< self.max_phrase_length:
+            possible_phrase_pairs = [phrase_pair for phrase_pair in self.l1_given_source if phrase_pair[0] == source]
+            cost = self.l1_given_source[phrase_pair] + self.ngrams[phrase_pair[0]]
+            #ngrams = source lm?
+        if i is j:
+            return -10 + self.
+        return -10000
+
 class Node:
     max_number_of_words=3 #max number of words in the history for language model
     stupid_backoff_rate=0.4 #rate for stupid backoff
-    previous_nodes=[]#links to thimport syse previous nodes
+    previous_nodes=[]#links to the previous nodes
     last_history=[] #last n-1 words in history 
     already_translated=[]#  numbers of words which were translated- coverage vector
     current_positions=[] #indexes of words in the source sentence which are currently translated,first number indicates the beginning of the phrase and second indicates the end pf phrase
